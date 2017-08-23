f0f9c796a4493e08e5c3948b3db0dd666a56f31d
Tuzov <dmy2zov@gmail.com>
2017-08-23 17:36:07 +0300

    checkout form done, top-nav, about-us page

--- ajax-email.php
+++ ajax-email.php
@@ -1,15 +1,28 @@
 <?php
-
+if ( ! session_id() ) @ session_start();
 /* SETTINGS */
 //$yourEmail = "itactiletrack@gmail.com";
 const Y_EMAIL = "itactiletrack@gmail.com";
 const EMAIL_S = "Tactile Track Preorder";
 
-if(!empty($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-    index();
-}else{
-     echo  json_encode('no email');
-}
+
+
+        if($_POST['action'] == 'index'){
+            if(!empty($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                index();
+                exit;
+            }else{
+                echo  json_encode('no email');
+                exit;
+            }
+
+        }
+
+        if($_POST['action'] == 'notes'){
+            notes();
+            exit;
+        }
+
 
 function is_injected($mess)
 // Проверяет, является ли $mess нормальным сообщением или это СПАМ
@@ -36,9 +49,13 @@ function index(){
         /* DATA FROM HTML FORM */
         $name = $_POST['name'];
         $email = $_POST['email'];
-        $notes = '54';
         $model = $_POST['mark'];
         $quantity = 1;
+        $_SESSION['name'] = $name;
+        $_SESSION['email'] = $email;
+        $_SESSION['model'] = $model;
+
+
         $headers = "From: $name <$email>\r\n" .
             "Reply-To: $name <$email>\r\n" .
             "Subject: ".EMAIL_S."\r\n" .
@@ -62,19 +79,15 @@ Email: '.$email.'
 Model: '.$model.'
 Quantity: '.$quantity.'
 
-Additional information:
-'.print_r($notes, TRUE)."
-
--- \r\n
 
 -----------------------------------------------------
 User-Agent:
-".$_SERVER['HTTP_USER_AGENT']."
+'.$_SERVER['HTTP_USER_AGENT'].'
 
-User Accept Language :".@$_SERVER['HTTP_ACCEPT_LANGUAGE']."
-User IP              :".@$_SERVER['REMOTE_ADDR']."
+User Accept Language :'.@$_SERVER['HTTP_ACCEPT_LANGUAGE'].'
+User IP              :'.@$_SERVER['REMOTE_ADDR'].'
 -----------------------------------------------------
-";
+';
 
 
         /* SEND EMAIL */
@@ -85,5 +98,66 @@ User IP              :".@$_SERVER['REMOTE_ADDR']."
             }
         }
 }
+
+
+function notes(){
+    /* DATA FROM HTML FORM */
+    $name =  $_SESSION['name'];
+    $email =  $_SESSION['email'];
+    $model = $_SESSION['model'];
+    $notes = $_POST['notes'];
+    $quantity = 1;
+    $headers = "From: $name <$email>\r\n" .
+        "Reply-To: $name <$email>\r\n" .
+        "Subject: ".EMAIL_S."\r\n" .
+        "Content-type: text/plain; charset=UTF-8\r\n" .
+        "MIME-Version: 1.0\r\n" .
+        "X-Mailer: PHP/" . phpversion() . "\r\n";
+
+    // защита от Mail-Injection***
+    $_is_injected=false;
+    if (is_injected($_POST["edt_Email"])){$_is_injected=true;};
+    if (is_injected($_POST["edt_Features"])){$_is_injected=true;};
+    if (is_injected($_POST["edt_Reason"])){$_is_injected=true;};
+
+
+
+    $message=
+        'New preorder of Tactile Track.
+
+Name: '.$name.'
+Email: '.$email.'
+Model: '.$model.'
+Quantity: '.$quantity.'
+
+
+Additional information:
+'.print_r($notes, TRUE).'
+
+-- \r\n
+-----------------------------------------------------
+User-Agent:
+'.$_SERVER['HTTP_USER_AGENT'].'
+
+User Accept Language :'.@$_SERVER['HTTP_ACCEPT_LANGUAGE'].'
+User IP              :'.@$_SERVER['REMOTE_ADDR'].'
+-----------------------------------------------------
+';
+
+
+    /* SEND EMAIL */
+    if ($_is_injected===false) {
+        if (mail(Y_EMAIL, EMAIL_S, $message, $headers))
+        {
+
+            echo  json_encode('ok');
+        }else{
+            echo  json_encode('error');
+            exit;
+        }
+    }
+}
+
+
 
 ?>
--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,82 @@
-setTimeout(function(){
+setTimeout(function () {
+	'use strict';
     document.getElementById("top-video").play();
 }, 8000);
+
+
+(function($) {
+
+    $('#name_s , #email_s').on('keyup',function(){
+
+         var name = $('#name_s').val();
+         var email = $('#email_s').val();
+
+        if(name.length >= 2 && email.length >= 5){
+            $('#btn-checkout').addClass('active');
+        }else {
+            $('#btn-checkout').removeClass('active');
+        }
+    });
+
+
+    $('#order-message').on('keyup',function(){
+
+         var message = $('#order-message').val();
+       
+        if(message.length >= 8){
+            $('#btn-checkout-2').addClass('active');
+        }else {
+            $('#btn-checkout-2').removeClass('active');
+        }
+    });
+
+
+ $(document).on('click', '.go_form', function(e){
+     e.preventDefault();
+
+    var name =  $("#name_s").val();
+    var email =  $("#email_s").val();
+    var mark =  $("#mark").val();
+
+    $.ajax({
+        url: '/ajax-email.php',
+        type: 'post',
+        data: ({  action:'index', name:name, email:email, mark:mark }),
+        dataType: 'json',
+        success: function (data) {
+            if(data == 'ok'){
+                $('#form_1').removeClass('active');
+                $('#form_2').addClass('active');
+            }
+            if(data == 'no email'){
+                $("#mes_err").empty().append('<p>Введите корректный E-mail</p>');
+            }
+        }
+    });
+});
+
+ $(document).on('click', '.go_form_2', function(e){
+     e.preventDefault();
+
+    var notes =  $("#order-message").val();
+
+
+    $.ajax({
+        url: '/ajax-email.php',
+        type: 'post',
+        data: ({  action:'notes', notes:notes}),
+        dataType: 'json',
+        success: function (data) {
+            if(data == 'ok'){
+
+                $('#form_2').empty().append('<h4>СПАСИБО ЗА ОБРАЩЕНИЕ, НАШИ МЕНЕДЖЕРЫ СВЯЖУТСЯ С ВАМИ В БЛИЖАЙШЕЕ ВРЕМЯ</h4>');
+                setTimeout(function() { $('.close').click(); }, 3000);
+            }
+            if(data == 'error'){
+                $("#mes_err").empty().append('<p>Сообщение не отправлено. Попробуйте позже</p>');
+            }
+        }
+    });
+});
+
+})(jQuery);
